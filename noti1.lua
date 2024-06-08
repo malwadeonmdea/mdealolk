c24bd57f6e881703200a6665fb930fffaea6eb74
malwadeonmdea <oplu18088@gmail.com>
2024-06-08 23:17:15 +0300

    Create noti1.lua

---
+++ noti1.lua
@@ -0,0 +1,21 @@
+local StarterGui = game:GetService("StarterGui")
+
+local Notifications = {
+	"Aimbot Enabled! RightClick",
+}
+
+local TimeBetweenNotifications = 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 --REPLACE 5 WITH THE NUMBER OF SECONDS YOU WANT TO WAIT IN BETWEEN NOTIFICATIONS APPEARING
+
+while true do
+	for i = 1, #Notifications do
+		local Notification = Notifications[i]
+		
+		StarterGui:SetCore("SendNotification", {
+			Title = "System Notification",
+			Text = Notification,
+			Duration = 5
+		})
+		
+		wait(TimeBetweenNotifications)
+	end
+end
